9a4272dba2015629ffa56703eaae4435540dd75c
Sanjay Mandadi <sanjay.mandadi@gmail.com>
2015-04-30 23:35:20 -0500

    Add readers for Events and EventSummary

---
+++ Mozu.Api.ToolKit/Readers/EventReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Event;
+using Mozu.Api.Resources.Event;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class EventReader : BaseReader
+    {
+        private EventCollection _results;
+        protected override async Task<bool> GetDataAsync()
+        {
+            var eventNotificationResource = new EventNotificationResource(Context);
+
+            _results = await eventNotificationResource.GetEventsAsync(startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, filter: Filter, responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<Event> Items
+        {
+            get { return _results.Items; }
+        } 
+    }
+}
---
+++ Mozu.Api.ToolKit/Readers/EventSummaryReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Event;
+using Mozu.Api.Resources.Event.Push.Subscriptions;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class EventSummaryReader : BaseReader
+    {
+        public string SubscriptionId { get; set; }
+        private EventDeliverySummaryCollection _results;
+        protected override async Task<bool> GetDataAsync()
+        {
+            var eventDeliverySummaryResource = new EventDeliverySummaryResource(Context);
+
+            if (String.IsNullOrEmpty(SubscriptionId))
+                throw new Exception("SubscriptionId is required for Get Event Delivery summary");
+
+
+            _results = await eventDeliverySummaryResource.GetDeliveryAttemptSummariesAsync(SubscriptionId,startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, filter: Filter, responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<EventDeliverySummary> Items
+        {
+            get { return _results.Items; }
+        } 
+    }
+}
