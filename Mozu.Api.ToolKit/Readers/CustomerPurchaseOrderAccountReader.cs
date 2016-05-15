0408a3bab2c9888728c5a0d84443c118219b31c0
Rakesh Dontula <rakesh_dontula@volusion.com>
2016-05-15 17:29:35 -0500

    Update SDK + PO readers

---
+++ Mozu.Api.ToolKit/Readers/CustomerPurchaseOrderAccountReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Customer;
+using Mozu.Api.Resources.Commerce.Customer;
+using Mozu.Api.Resources.Commerce.Customer.Accounts;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class CustomerPurchaseOrderAccountReader : BaseReader
+    {
+        private CustomerPurchaseOrderAccountCollection _results;
+      
+        protected async override Task<bool> GetDataAsync()
+        {
+            var resource = new CustomerAccountResource(Context);
+            _results =await resource.GetCustomersPurchaseOrderAccountsAsync(startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<CustomerPurchaseOrderAccount> Items
+        {
+            get { return _results.Items; }
+        }
+    }
+}
---
+++ Mozu.Api.ToolKit/Readers/CustomerPurchaseOrderAccountTransactionReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Customer;
+using Mozu.Api.Resources.Commerce.Customer.Accounts;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class CustomerPurchaseOrderAccountTransactionReader : BaseReader
+    {
+        private PurchaseOrderTransactionCollection _results;
+        public int AccountId { get; set; }
+        protected async override Task<bool> GetDataAsync()
+        {
+            var resource = new CustomerPurchaseOrderAccountResource(Context);
+            _results = await resource.GetCustomerPurchaseOrderTransactionsAsync(AccountId, startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy,responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<PurchaseOrderTransaction> Items
+        {
+            get { return _results.Items; }
+        }
+    }
+}
