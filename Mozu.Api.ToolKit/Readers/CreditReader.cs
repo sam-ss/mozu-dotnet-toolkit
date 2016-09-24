3c561d32b909121efb2b4757cb852780e4dd174e
Sanjay Mandadi <sanjay_mandadi@volusion.com>
2016-09-24 15:38:47 -0500

    Add store credit readers

---
+++ Mozu.Api.ToolKit/Readers/CreditReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Customer.Credit;
+using Mozu.Api.Resources.Commerce.Customer;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class CreditReader : BaseReader
+    {
+        private CreditCollection _results = null;
+
+        protected override async Task<bool> GetDataAsync()
+        {
+            var resource = new CreditResource(Context);
+            _results = await resource.GetCreditsAsync(startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, filter: Filter, responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<Credit> Items
+        {
+            get { return _results.Items; }
+        }
+    }
+}
---
+++ Mozu.Api.ToolKit/Readers/CreditTransactionReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.Customer.Credit;
+using Mozu.Api.Resources.Commerce.Customer.Credits;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class CreditTransactionReader : BaseReader
+    {
+        private CreditTransactionCollection _results = null;
+        public string Code { get; set; }
+        protected override async Task<bool> GetDataAsync()
+        {
+            var resource = new CreditTransactionResource(Context);
+            _results = await resource.GetTransactionsAsync(Code,startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, filter: Filter,  responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<CreditTransaction> Items
+        {
+            get { return _results.Items; }
+        }
+    }
+}
