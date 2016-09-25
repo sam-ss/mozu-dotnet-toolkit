1ebcf0038bf0b3f609ae9037240432ad056c53a8
Sanjay Mandadi <sanjay_mandadi@volusion.com>
2016-09-24 22:52:12 -0500

    Add ProductReservation reader

---
+++ Mozu.Api.ToolKit/Readers/ProductReservationsReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mozu.Api.Contracts.ProductAdmin;
+using Mozu.Api.Resources.Commerce.Catalog.Admin;
+
+namespace Mozu.Api.ToolKit.Readers
+{
+    public class ProductReservationsReader : BaseReader
+    {
+        private ProductReservationCollection _results = null;
+
+        protected override async Task<bool> GetDataAsync()
+        {
+            var resource = new ProductReservationResource(Context);
+            _results = await resource.GetProductReservationsAsync(startIndex: StartIndex, pageSize: PageSize, sortBy: SortBy, filter: Filter, responseFields: ResponseFields);
+
+            TotalCount = _results.TotalCount;
+            PageCount = _results.PageCount;
+            PageSize = _results.PageSize;
+            return _results.Items != null && _results.Items.Count > 0;
+        }
+
+        public List<ProductReservation> Items
+        {
+            get { return _results.Items; }
+        }
+    }
+}
